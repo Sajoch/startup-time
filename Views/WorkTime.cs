8ddcea096b299e89caa8dc40c3a33fb5d179b744
Sajoch <pojtrot@gmail.com>
2022-03-23 09:06:58 +0100

    notify on overwork

--- Views/WorkTime.cs
+++ Views/WorkTime.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Resources;
 using System.Windows.Forms;
 
 namespace startup_timer {
     class WorkTime : IObserver<TimeContainer> {
         private readonly TimeContainer container;
         private readonly NotifyWidget widget;
+        private readonly ResourceManager resource = new ResourceManager(typeof(Resources.Resource));
+        private bool isAlertShown = false;
 
         public WorkTime(TimeContainer container, NotifyWidget widget) {
             this.container = container;
@@ -21,10 +24,24 @@ namespace startup_timer {
         public void OnNext(TimeContainer value) {
             widget.SetOverworkBadge(value.IsTimeElapsed);
             widget.SetInfo(value.ShortFormat);
+
+            if (!isAlertShown && value.IsTimeElapsed) {
+                ShowOverworkAlert();
+                isAlertShown = true;
+            } else if (isAlertShown && !value.IsTimeElapsed)
+                isAlertShown = false;
         }
 
         void ShowInfo() {
-            MessageBox.Show(container.GetInfo(), "Startup-Timer");
+            var caption = resource.GetString("appName");
+            MessageBox.Show(container.GetInfo(), caption);
+        }
+
+        void ShowOverworkAlert() {
+            var caption = resource.GetString("appName");
+            var format = resource.GetString("overworkAlert");
+            var message = string.Format(format, container.ShortWorkTime);
+            MessageBox.Show(message, caption);
         }
     }
 }
